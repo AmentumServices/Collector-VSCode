0e97db5b4688da6daeae2d9e69ea807ec06cad67
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2024-06-19 21:31:21 -0600

    Update gh_latest.sh script for better output

--- gh_latest.sh
+++ gh_latest.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-REPO=JacobsFederal/$(basename "$PWD")
-DEST="~/Downloads/$SRC"
+SRC=$(basename "$PWD")
+REPO="JacobsFederal/$SRC"
+DEST="$HOME/Downloads/$SRC"
 
-echo "Working with github repo $REPO"
-echo "Listing releases"
+echo -e "\nWorking with github repo $REPO"
+echo -e "Listing releases\n"
 gh release list -R $REPO
-echo "Downloading Latest to $DEST"
-gh release download -p "*" \
-  -D $DEST -R $REPO
+echo -e "\nDownloading Latest to $DEST\n"
+gh release download -p "*" -D $DEST -R $REPO
+ls -Alht $DEST 
+if [ $? ];then
+  echo -e "\nDownload Complete!"
+else
+  echo "ERROR!"
+fi
